9c9e32a34b67a28776a10db94666ca754ed00345
vvviiitttaaalll123 <vitalblaze2@gmail.com>
2022-12-02 22:16:50 +0530

    first commit

---
+++ app/constants/constants.py
@@ -0,0 +1,47 @@
+API_BASE_URL = 'https://api.mangadex.org/'
+ORG_BASE_URL = 'https://mangadex.org/'
+
+GET_SEASONAL_UPDATES = f'{API_BASE_URL}list/4be9338a-3402-4f98-b467-43fb56663927?includes[]=user'
+GET_RECENTLY_UPDATED = f'{API_BASE_URL}chapter?includes[]=manga&includes[]=scanlation_group&translatedLanguage[' \
+                       f']=en&contentRating[]=safe&contentRating[]=suggestive&contentRating[]=erotica&order[' \
+                       f'readableAt]=desc&offset=0&limit=24'
+
+GET_RECENTLY_ADDED = f'{API_BASE_URL}manga?limit=30&contentRating[]=safe&contentRating[]=suggestive&' \
+                     'contentRating[]=erotica&order[createdAt]=desc&includes[]=cover_art'
+
+
+def GET_COVER_URL(manga_id: str) -> str:
+    return f'{API_BASE_URL}cover?limit=1&offset=0&manga%5B%5D={manga_id}&order%5BcreatedAt%5D=' \
+           f'asc&order%5BupdatedAt%5D=asc&order%5Bvolume%5D=asc'
+
+
+MANGA_DETAILS_URL_BUILD = f'{API_BASE_URL}manga?includes[]=cover_art&order[followedCount]=desc' \
+                          f'&contentRating[]=safe&contentRating[]=suggestive&contentRating[' \
+                          f']=erotica&hasAvailableChapters=true&ids[]='
+
+
+def IMAGE_URL(manga_id: str, image_url: str, bit: str = ".256.jpg") -> str:
+    return f'{ORG_BASE_URL}covers/${manga_id}/${image_url}${bit}'
+
+
+def PERTICULAR_MANGA(manga_id: str) -> str:
+    return f'{API_BASE_URL}manga/{manga_id}?includes[]=artist&includes[]=author&includes[]=cover_art'
+
+
+def GET_CHAPTERS(manga_id: str, limit: int = 96, offset: int = 0) -> str:
+    return f'{API_BASE_URL}manga/{manga_id}/feed?translatedLanguage[]=en&limit={limit}&includes[]=scanlation_group' \
+           f'&includes[]=user&order[volume]=desc&order[chapter]=desc&offset={offset}&contentRating[' \
+           f']=safe&contentRating[]=suggestive&contentRating[]=erotica&contentRating[]=pornographic'
+
+
+def GET_CHAPTER_IMAGES(chapter_id: str) -> str:
+    return f'{API_BASE_URL}/at-home/server/{chapter_id}?forcePort443=false'
+
+
+def GET_CHAPTER_DETAIL(chapter_id: str) -> str:
+    return f'{API_BASE_URL}chapter/{chapter_id}?includes[]=scanlation_group&includes[]=manga&includes[]=user'
+
+
+def SEARCH_URL(title: str) -> str:
+    return f'{API_BASE_URL}manga?title={title}&limit=5&contentRating[]=safe&contentRating[]=suggestive&contentRating[' \
+           f']=erotica&includes[]=cover_art&order[relevance]=desc '
---
+++ app/views.py
@@ -0,0 +1,86 @@
+from app import app
+import requests
+from app.constants.constants import *
+from flask import request
+
+
+@app.route("/get_seasonal_updates", methods=['GET'])
+def get_seasonal_updates():
+    req = requests.get(GET_SEASONAL_UPDATES)
+    return req.json()
+
+
+@app.route("/get_recently_updated", methods=['GET'])
+def get_recently_updated():
+    req = requests.get(GET_RECENTLY_UPDATED)
+    return req.json()
+
+
+@app.route("/get_recently_added", methods=['GET'])
+def get_recently_added():
+    req = requests.get(GET_RECENTLY_ADDED)
+    return req.json()
+
+
+@app.route("/get_cover", methods=['GET'])
+def get_cover():
+    req = requests.get(GET_COVER_URL(request.args.get('manga_id')))
+    return req.json()
+
+
+@app.route("/get_manga_details", methods=['POST'])
+def get_manga_details_url():
+    data = request.json
+    url = MANGA_DETAILS_URL_BUILD
+
+    for ids in data['ids']:
+        url += ids + "&ids[]="
+
+    req = requests.get(url[:-7])
+    return req.json()
+
+
+@app.route("/get_image_url", methods=['GET'])
+def get_image_url():
+    manga_id = request.args.get('manga_id')
+    image_url = request.args.get('image_url')
+    bit = request.args.get('bit', default=".256.jpg")
+    req = requests.get(IMAGE_URL(manga_id, image_url, bit))
+    return req.json()
+
+
+@app.route("/manga/<manga_id>", methods=['GET'])
+def get_particular_manga(manga_id: str):
+    req = requests.get(PERTICULAR_MANGA(manga_id))
+    return req.json()
+
+
+@app.route("/get_chapters", methods=['GET'])
+def get_chapters():
+    manga_id = request.args.get("manga_id")
+    limit = int(request.args.get('limit'))
+    offset = int(request.args.get('offset'))
+
+    req = requests.get(GET_CHAPTERS(manga_id, limit, offset))
+    return req.json()
+
+
+@app.route("/get_chapter_images", methods=['GET'])
+def get_chapter_images():
+    chapter_id = request.args.get('chapter_id')
+    req = requests.get(GET_CHAPTER_IMAGES(chapter_id))
+    return req
+
+
+@app.route("/get_chapter_detail", methods=['GET'])
+def get_chapter_detail():
+    chapter_id = request.args.get('chapter_id')
+    req = requests.get(GET_CHAPTER_DETAIL(chapter_id))
+    return req.json()
+
+
+@app.route("/search", methods=['GET'])
+def search_url():
+    title = request.args.get('title')
+    req = requests.get(SEARCH_URL(title))
+    return req.json()
